7a277e8541ad246d1ce6290d41d809be90ae2ad3
Vadym Shkarbul <vadim.shkarbul@gmail.com>
2022-11-10 17:58:15 +0200

    Added verbose names for Dish and DishType models

--- kitchen/models.py
+++ kitchen/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import AbstractUser
 
 
 class Cook(AbstractUser):
-    years_of_experience = models.IntegerField()
+    years_of_experience = models.IntegerField(null=True)
 
     def __str__(self):
         return f"{self.first_name} {self.last_name} ({self.years_of_experience} YOE)"
@@ -16,6 +16,10 @@ class Cook(AbstractUser):
 class DishType(models.Model):
     name = models.CharField(max_length=255, unique=True)
 
+    class Meta:
+        verbose_name = "dish type"
+        verbose_name_plural = "dishes types"
+
     def __str__(self):
         return self.name
 
@@ -26,6 +30,10 @@ class Dish(models.Model):
     price = models.DecimalField(max_digits=6, decimal_places=2)
     dish_type = models.ForeignKey(DishType, on_delete=models.CASCADE)
     cooks = models.ManyToManyField(Cook, related_name="cooks")
+
+    class Meta:
+        verbose_name = "dish"
+        verbose_name_plural = "dishes"
 
     def __str__(self):
         return f"{self.name} ({self.price})"
