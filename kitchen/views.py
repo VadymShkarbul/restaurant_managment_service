82c8dc6de2cdac87581321ff77cf9278fcd77286
Vadym Shkarbul <vadim.shkarbul@gmail.com>
2022-11-10 18:30:58 +0200

    Added base.html and index.html templates and index function

--- kitchen/views.py
+++ kitchen/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
-# Create your views here.
+from kitchen.models import Cook, Dish, DishType
+
+
+def index(request):
+    num_cooks = Cook.objects.count()
+    num_dish_type = DishType.objects.count()
+    num_dish = Dish.objects.count()
+
+    context = {
+        "num_cooks": num_cooks,
+        "num_dish_type": num_dish_type,
+        "num_dish": num_dish,
+    }
+
+    return render(request, "kitchen/index.html", context=context)
