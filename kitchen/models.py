c11b626a1f140807f375b382bd023ef9214b1749
Vadym Shkarbul <vadim.shkarbul@gmail.com>
2022-11-11 01:56:52 +0200

    Added unique option for DishType field

--- kitchen/models.py
+++ kitchen/models.py
@@ -29,7 +29,7 @@ class DishType(models.Model):
 
 
 class Dish(models.Model):
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=255, unique=True)
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
     dish_type = models.ForeignKey(DishType, on_delete=models.CASCADE)
