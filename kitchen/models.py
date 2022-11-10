76fcd3270496b8d2493b64ffe4d23e3088ac2842
Vadym Shkarbul <vadim.shkarbul@gmail.com>
2022-11-10 18:15:01 +0200

    Added verbose name for Cook model and customise admin page

--- kitchen/models.py
+++ kitchen/models.py
@@ -6,6 +6,10 @@ from django.contrib.auth.models import AbstractUser
 class Cook(AbstractUser):
     years_of_experience = models.IntegerField(null=True)
 
+    class Meta:
+        verbose_name = "cook"
+        verbose_name_plural = "cooks"
+
     def __str__(self):
         return f"{self.first_name} {self.last_name} ({self.years_of_experience} YOE)"
 
