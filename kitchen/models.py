0b5214914cd93c2124d8c3419c4bc05a59f21833
Vadym Shkarbul <vadim.shkarbul@gmail.com>
2022-11-10 15:07:42 +0200

    Modified models.py and app name added to settings.py

--- kitchen/models.py
+++ kitchen/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+# from django.urls import reverse
 
-# Create your models here.
+
+class Cook(AbstractUser):
+    years_of_experience = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name} ({self.years_of_experience} YOE)"
+
+    # def get_absolute_url(self):
+    #     return reverse("", kwargs={"pk": self.pk})
+
+
+class DishType(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Dish(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.DecimalField()
+    dish_type = models.ForeignKey(DishType, on_delete=models.CASCADE)
+    cooks = models.ManyToManyField(Cook, related_name="cooks")
+
+    def __str__(self):
+        return f"{self.name} ({self.price})"
