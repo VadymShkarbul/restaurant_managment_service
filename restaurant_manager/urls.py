091a800b1d30c6c46409ff5579400a934cb46704
Vadym Shkarbul <vadim.shkarbul@gmail.com>
2022-11-10 22:41:13 +0200

    Added styles.css

--- restaurant_manager/urls.py
+++ restaurant_manager/urls.py
@@ -1,7 +1,9 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("kitchen.urls", namespace="kitchen")),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
