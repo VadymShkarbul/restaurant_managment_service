7e8e921baad7237f2b99cbe38f0f79dc0604c03a
Vadym Shkarbul <vadim.shkarbul@gmail.com>
2022-11-11 02:13:36 +0200

    Added dish detail view

--- kitchen/urls.py
+++ kitchen/urls.py
@@ -4,7 +4,12 @@ from kitchen.views import (index,
                            DishTypeListView,
                            DishTypeCreateView,
                            DishTypeUpdateView,
-                           DishTypeDeleteView, DishListView, DishCreateView, DishUpdateView, DishDeleteView)
+                           DishTypeDeleteView,
+                           DishDetailView,
+                           DishListView,
+                           DishCreateView,
+                           DishUpdateView,
+                           DishDeleteView,)
 
 urlpatterns = [
     path("", index, name="index"),
@@ -13,6 +18,7 @@ urlpatterns = [
     path("dishes_types/update/<int:pk>/", DishTypeUpdateView.as_view(), name="dish-type-update"),
     path("dishes_types/delete/<int:pk>/", DishTypeDeleteView.as_view(), name="dish-type-delete"),
     path("dishes/", DishListView.as_view(), name="dish-list"),
+    path("dishes/<int:pk>/", DishDetailView.as_view(), name="dish-detail"),
     path("dishes/create/", DishCreateView.as_view(), name="dish-create"),
     path("dishes/update/<int:pk>/", DishUpdateView.as_view(), name="dish-update"),
     path("dishes/delete/<int:pk>/", DishDeleteView.as_view(), name="dish-delete"),
